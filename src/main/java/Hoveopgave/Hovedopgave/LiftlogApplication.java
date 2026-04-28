84395f9b230332d011d17d1f1dd6ffe11d12d9d0
montif16 <Lukas1532@hotmail.com>
2026-04-28 11:19:57 +0200

    React add and name changes.

--- src/main/java/Hoveopgave/Hovedopgave/LiftlogApplication.java
+++ src/main/java/Hoveopgave/Hovedopgave/LiftlogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HovedopgaveApplication {
+public class LiftlogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HovedopgaveApplication.class, args);
+		SpringApplication.run(LiftlogApplication.class, args);
 	}
 
 }
