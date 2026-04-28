d18e0308bf4becc9c19471c47736bdbaedf26053
montif16 <Lukas1532@hotmail.com>
2026-04-28 10:52:58 +0200

    Initial commit

---
+++ src/main/java/Hoveopgave/Hovedopgave/HovedopgaveApplication.java
@@ -0,0 +1,13 @@
+package Hoveopgave.Hovedopgave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HovedopgaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HovedopgaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hoveopgave/Hovedopgave/HovedopgaveApplicationTests.java
@@ -0,0 +1,13 @@
+package Hoveopgave.Hovedopgave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HovedopgaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
